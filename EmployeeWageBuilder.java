e94f52627de0bc8b13abd36c97de116c54cb5830
purushothaman-source <purushoth99jun@gmail.com>
2021-01-11 08:14:01 +0530

    ADDED :code to calculate EmployeeWage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -6,10 +6,17 @@ public final class EmployeeWageBuilder {
     public static void main(String[] args) {
         final int Is_Full_Time = 1;
         Random random = new Random();
+        int empRatePerHr = 20;
+        int empHrs;
         int empCheck = random.nextInt(2);
-        if (empCheck == Is_Full_Time)
+        if (empCheck == Is_Full_Time) {
             System.out.println("Employee is present");
-        else
+            empHrs = 8;
+        } else {
             System.out.println("Employee is absent");
+            empHrs = 0;
+        }
+        int salary = empHrs * empRatePerHr;
+        System.out.println("Salary : " + salary);
     }
 }
