9c494881933c0e9b8abf7e0eeffe64c7a99cadd1
purushothaman-source <purushoth99jun@gmail.com>
2021-01-11 07:58:06 +0530

    ADDED :functionality to print welcome message

---
+++ EmployeeWageBuilder.java
@@ -0,0 +1,8 @@
+package com.uccases;
+
+public final class EmployeeWageBuilder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee wage computation");
+    }
+}
+
