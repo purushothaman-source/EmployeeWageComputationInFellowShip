4a5ffff8ca06e1556dbb2f1b052a3a78fa0aa5ff
purushothaman-source <purushoth99jun@gmail.com>
2021-01-11 08:04:19 +0530

    ADDED :code to check employee attendence

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,8 +1,15 @@
 package com.uccases;
 
+import java.util.Random;
+
 public final class EmployeeWageBuilder {
     public static void main(String[] args) {
-        System.out.println("Welcome to employee wage computation");
+        final int Is_Full_Time = 1;
+        Random random = new Random();
+        int empCheck = random.nextInt(2);
+        if (empCheck == Is_Full_Time)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
     }
 }
-
