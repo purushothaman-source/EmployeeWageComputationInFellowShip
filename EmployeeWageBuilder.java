704b377582e5f4e3d87a5fb34be01b77996053f9
purushothaman-source <purushoth99jun@gmail.com>
2021-01-11 08:21:49 +0530

    ADDED :code to add part time emphrs

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -5,14 +5,19 @@ import java.util.Random;
 public final class EmployeeWageBuilder {
     public static void main(String[] args) {
         final int Is_Full_Time = 1;
+        final int Is_Part_Time = 2;
         Random random = new Random();
         int empRatePerHr = 20;
         int empHrs;
-        int empCheck = random.nextInt(2);
+        int empCheck = random.nextInt(3);
         if (empCheck == Is_Full_Time) {
             System.out.println("Employee is present");
             empHrs = 8;
-        } else {
+        }else if(empCheck==Is_Part_Time){
+            System.out.println("Employee is Part Time");
+            empHrs=4;
+        }
+        else {
             System.out.println("Employee is absent");
             empHrs = 0;
         }
