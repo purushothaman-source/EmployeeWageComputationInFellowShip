4f35a9b461b66b1afa4a2fc9c4e650ee4e7f6243
purushothaman-source <purushoth99jun@gmail.com>
2021-01-11 08:42:45 +0530

    ADDED : code to calculate EmployeeWage for a month

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -3,27 +3,31 @@ package com.uccases;
 import java.util.Random;
 
 public final class EmployeeWageBuilder {
+
     public static void main(String[] args) {
         final int Is_Full_Time = 1;
         final int Is_Part_Time = 2;
         Random random = new Random();
         int empRatePerHr = 20;
-        int empHrs;
-        int empCheck = random.nextInt(3);
-        switch (empCheck) {
-            case Is_Full_Time:
-                System.out.println("Employee is present");
-                empHrs = 8;
-                break;
-            case Is_Part_Time:
-                System.out.println("Employee is Part Time");
-                empHrs = 4;
-                break;
-            default:
-                System.out.println("Employee is absent");
-                empHrs = 0;
+        int empHrs = 0;
+        int totalEmpHrs = 0;
+
+        for(int i=1;i<=20;i++) {
+            int empCheck = random.nextInt(3);
+            switch (empCheck) {
+                case Is_Full_Time:
+                    empHrs = 8;
+                    break;
+                case Is_Part_Time:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            totalEmpHrs += empHrs;
         }
-        int salary = empHrs * empRatePerHr;
+        System.out.println("TotalEmpHrs : "+totalEmpHrs);
+        int salary = totalEmpHrs * empRatePerHr;
         System.out.println("Salary : " + salary);
     }
 }
