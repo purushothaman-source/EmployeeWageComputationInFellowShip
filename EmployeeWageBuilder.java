fb7098a8235470c44907ada8e731e93b14ec51eb
purushothaman-source <purushoth99jun@gmail.com>
2021-01-12 11:58:14 +0530

    ADDED :code to compute multiple company wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -5,18 +5,19 @@ import java.util.Random;
 public final class EmployeeWageBuilder {
 
     public static void main(String[] args) {
-        computeEmpWage();
+        computeEmpWage("TCS",30,20,10);
+        computeEmpWage("ACCENTURE",100,20,10);
+
     }
-    public static int computeEmpWage(){
+    public static void computeEmpWage(String company, int MAX_WORKING_HRS, int NO_OF_WORKING_DAYS, int RATE_PER_HR){
         final int Is_Full_Time = 1;
         final int Is_Part_Time = 2;
         Random random = new Random();
         final int empRatePerHr = 20;
         int empHrs = 0;
         int totalEmpHrs = 0;
-        final int totalWorkingDays = 20;
         int workingDays = 0;
-        while (workingDays < totalWorkingDays && empHrs <= totalEmpHrs) {
+        while (workingDays < NO_OF_WORKING_DAYS && totalEmpHrs <= MAX_WORKING_HRS) {
             int empCheck = random.nextInt(3);
             workingDays++;
             switch (empCheck) {
@@ -34,7 +35,7 @@ public final class EmployeeWageBuilder {
         }
         System.out.println("TotalEmpHrs : " + totalEmpHrs);
         int salary = totalEmpHrs * empRatePerHr;
-        System.out.println("Salary : " + salary);
-        return salary;
+        System.out.println("COMPANY :"+company +"Salary : " + salary);
+
     }
 }
